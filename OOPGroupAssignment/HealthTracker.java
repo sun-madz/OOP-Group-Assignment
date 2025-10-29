1bcc548377514c9066279ed52375cadcf2be972a
Sun <123832655+sun-madz@users.noreply.github.com>
2025-10-29 12:06:25 +0400

    Add files via upload

---
+++ OOPGroupAssignment/HealthTracker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package OOPGroupAssignment;
+
+/**
+ *
+ * @author sandr
+ */
+public class HealthTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
