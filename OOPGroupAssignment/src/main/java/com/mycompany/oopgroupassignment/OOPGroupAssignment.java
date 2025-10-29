1ba53c3fa8f6118399c46df67170978927024e8d
Sun <123832655+sun-madz@users.noreply.github.com>
2025-10-29 11:51:10 +0400

    Add files via upload

---
+++ OOPGroupAssignment/src/main/java/com/mycompany/oopgroupassignment/OOPGroupAssignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oopgroupassignment;
+
+/**
+ *
+ * @author sandr
+ */
+public class OOPGroupAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
